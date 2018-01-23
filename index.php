0bd563ca692d022fbb06e078997105c6aa6b8be1
KRAGBE <angekragbe@gmail.com>
2018-01-23 10:35:39 +0100

    index changed

--- index.php
+++ index.php
@@ -16,15 +16,18 @@
 		
 			<div>
 			<?php 
+				if (!isset($_SESSION['mail'])) {
 				echo '<a href="les_pages/connexion.php"><button  type="">connexion</button></a>
-				<a href="les_pages/deconnexion.php"><button  type="">deconnexion</button></a>
 				<a href="les_pages/inscription.php"><button type="">inscription</button></a> 
 				<a href="les_pages/rechercheTrajet.php"><button type="">rechercher un trajet</button></a>
 				<a href="les_pages/proposerTrajet.php"><button type="">Proposer un trajet</button></a> '; 
-
+			}
 				// on affiche l'option profil à condition qu'il ai eu connexion et donc qu'une adresse mail est été renseignée
 				if (isset($_SESSION['mail'])) { 
-					echo '<a href="les_pages/profil.php"><button  type="">Profil</button></a>';
+					echo '<a href="les_pages/rechercheTrajet.php"><button type="">rechercher un trajet</button></a>
+						  <a href="les_pages/proposerTrajet.php"><button type="">Proposer un trajet</button></a>
+						  <a href="les_pages/deconnexion.php"><button  type="">deconnexion</button></a>
+						  <a href="les_pages/profil.php"><button  type="">Profil</button></a>';
 					echo $_SESSION['mail'];
 					//$_SESSION['mail']=null;
 				}
